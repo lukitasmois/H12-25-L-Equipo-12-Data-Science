328be279a5df1855d1902fb2727ecfdc59e28e20
lukitasmois <lmoix85@gmail.com>
2026-01-04 05:34:34 -0300

    edite el backend en springboot para conectarlo con el backend en fastapi

--- backend/Backend_SpringBoot/src/com/flightontime/api/service/PredictionService.java
+++ backend/Backend_SpringBoot/src/com/flightontime/api/service/PredictionService.java
@@ -3,6 +3,9 @@ package com.flightontime.api.service;
 import com.flightontime.api.dto.PredictionResponse;
 import com.flightontime.api.dto.FlightRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,26 +21,28 @@ public class PredictionService {
         this.restTemplate = restTemplate;
     }
 
-    /**
-     * Envía los datos del vuelo al microservicio de Python y retorna la predicción.
-     * MODIFICACIÓN: Se cambió el orden de las clases para que coincida con el Controller.
-     */
-    public PredictionResponse getPrediction(FlightRequest request) { // <-- Cambiado: Recibe FlightRequest
+    public PredictionResponse getPrediction(FlightRequest request) {
 
-        // --- INICIO DE BLOQUE MOCK (PARA TESTEO LOCAL) ---
-        // Comenta este bloque cuando quieras usar la IA real en FastAPI
-        PredictionResponse respuestaMock = new PredictionResponse();
-        respuestaMock.setPrevision("On Time");
-        respuestaMock.setProbabilidad(0.95);
-        return respuestaMock;
-        // --- FIN DE BLOQUE MOCK ---
+        String endpoint = dsApiUrl;
 
-        /*
-        // LÓGICA REAL PARA FAST API (Se activará después)
-        String endpoint = dsApiUrl + "/predict";
+        //configure header para que fastapi entienda el json
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // Enviamos "request" (FlightRequest) y esperamos 'PredictionResponse.class'
-        return restTemplate.postForObject(endpoint, request, PredictionResponse.class);
-        */
+        //creo la peticion
+        HttpEntity<FlightRequest> entity = new HttpEntity<>(request, headers);
+
+        try {
+            PredictionResponse response = restTemplate.postForObject(endpoint, entity, PredictionResponse.class);
+            System.out.println("response: " + response);
+            return response;
+        } catch (Exception e) {
+            System.err.println("Error conectando con la API de DS: " + e.getMessage());
+
+            PredictionResponse errorRes = new PredictionResponse();
+            errorRes.setPrevision("Error de conexión");
+            errorRes.setProbabilidad(0.0);
+            return errorRes;
+        }
     }
 }
